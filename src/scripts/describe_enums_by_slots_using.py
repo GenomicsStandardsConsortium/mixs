1056d6891c568406cc3521556dd946d619bb114c
Mark A. Miller <MAM@lbl.gov>
2024-08-13 15:53:26 -0400

    with enum descriptions

---
+++ src/scripts/describe_enums_by_slots_using.py
@@ -0,0 +1,22 @@
+from linkml_runtime import SchemaView
+from linkml_runtime.dumpers import yaml_dumper
+
+schema_file = "../mixs/schema/mixs.yaml"  # if being run from the src/scripts directory
+schema_view = SchemaView(schema_file)
+
+schema_enums = schema_view.all_enums()
+
+for ek, ev in schema_enums.items():
+    users = schema_view.get_slots_by_enum(ek)
+    user_names = [u.name for u in users]
+    user_names.sort()
+    if len(user_names) != 1:
+        print(f"Enum: {ek} used by: {len(user_names)} slots: {user_names}")
+    if len(user_names) == 0:
+        ev.description = f"permissible values, not used by any term"
+    elif len(user_names) == 1:
+        ev.description = f"permissible values, used term {user_names[0]}"
+    else:
+        ev.description = f"permissible values, used by {len(user_names)} terms: {', '.join(user_names)}"
+
+yaml_dumper.dump(schema_view.schema, "mixs_with_enum_descriptions.yaml")
