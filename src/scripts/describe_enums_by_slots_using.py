0ed434f41059dc87b9ae8127906731450d32d8aa
Sujay Patil <sujaysanjeev.patil@gmail.com>
2024-09-18 11:03:28 -0700

    modified boilerplate text for enum descriptions

--- src/scripts/describe_enums_by_slots_using.py
+++ src/scripts/describe_enums_by_slots_using.py
@@ -1,7 +1,7 @@
 from linkml_runtime import SchemaView
 from linkml_runtime.dumpers import yaml_dumper
 
-schema_file = "../mixs/schema/mixs.yaml"  # if being run from the src/scripts directory
+schema_file = "src/mixs/schema/mixs.yaml"  # if being run from the src/scripts directory
 schema_view = SchemaView(schema_file)
 
 schema_enums = schema_view.all_enums()
@@ -15,7 +15,7 @@ for ek, ev in schema_enums.items():
     if len(user_names) == 0:
         ev.description = f"permissible values, not used by any term"
     elif len(user_names) == 1:
-        ev.description = f"permissible values, used term {user_names[0]}"
+        ev.description = f"permissible values, used by term {user_names[0]}"
     else:  # len(user_names) > 1
         ev.description = f"permissible values, used by {len(user_names)} terms: {', '.join(user_names)}"
 
